0b76f36bd4d14f2c549f62570f95834fd3f02e78
Diego Gomez <Diego.Gomez@pilsac.com>
2024-10-31 14:46:25 -0600

    Se creo un endpoint para generar sudoku

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,29 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Services\SudokuGenerator;
 
-Route::get('/', function (Request $request) {
-    $failedResults = [];
+Route::get('/sudoku/generate', function (Request $request) {
 
-    for ($i = 0; $i < 1000; $i++) {
-        $generator = new \App\Services\SudokuGenerator();
-        $board = $generator->generate();
-
-        $sudokuWithRemovedNumbers = $generator->removeNumbers(40);
-
-        $validator = new \App\Services\SudokuValidator($sudokuWithRemovedNumbers);
-        $isValid = $validator->isValid();
-
-        if (!$isValid) {
-            $failedResults[] = [
-                'original_board' => $board,
-                'puzzle_board' => $sudokuWithRemovedNumbers
-            ];
-        }
-    }
+    $generator = new SudokuGenerator();
+    $board = $generator->generate();
+    $sudokuWithRemovedNumbers = $generator->removeNumbers(40);
 
     return response()->json([
-        'failed_count' => count($failedResults),
-        'failed_results' => $failedResults
+        'solution' => $board,
+        'sudoku' => $sudokuWithRemovedNumbers
     ]);
 });
