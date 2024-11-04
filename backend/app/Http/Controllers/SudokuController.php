49072bc0ca9f543a506c5e3e280a86643e18aa48
Diego Gomez <Diego.Gomez@pilsac.com>
2024-11-04 11:39:52 -0600

    Se crea un sudoku en la DB al generarlo

--- backend/app/Http/Controllers/SudokuController.php
+++ backend/app/Http/Controllers/SudokuController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Enums\SudokuDifficult;
 use App\Http\Requests\GenerateSudokuRequest;
+use App\Models\Sudoku;
 use App\Services\SudokuGenerator;
 use Illuminate\Http\JsonResponse;
 
@@ -11,11 +12,17 @@ class SudokuController extends Controller
 {
     public function generateSudoku(GenerateSudokuRequest $request): JsonResponse
     {
+        $difficult = SudokuDifficult::from($request->difficult);
+
         $generator = new SudokuGenerator();
         $board = $generator->generate();
-
-        $difficult = SudokuDifficult::from($request->difficult);
         $sudokuWithRemovedNumbers = $generator->removeNumbers($difficult);
+
+        $newSudoku = new Sudoku();
+        $newSudoku->solution = json_encode($board);
+        $newSudoku->grid = json_encode($sudokuWithRemovedNumbers);
+        $newSudoku->difficult = $difficult;
+        $newSudoku->save();
 
         return response()->json([
             'solution' => $board,
