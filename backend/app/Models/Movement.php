f7ec0eafbb13212086117146b64de84818a1fc6e
Diego Gomez <Diego.Gomez@pilsac.com>
2024-11-04 14:41:49 -0600

    Se creo la tabla de movimientos

---
+++ backend/app/Models/Movement.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movement extends Model
+{
+    protected $fillable = [
+        'game_id',
+        'current_grid',
+        'number_movement',
+        'is_winning_movement',
+        'is_backward'
+    ];
+}
---
+++ backend/database/migrations/2024_11_04_202121_create_movements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('game_id')->constrained('games')->cascadeOnDelete();
+            $table->json('current_grid');
+            $table->integer('number_movement');
+            $table->boolean('is_winning_movement')->default(false);
+            $table->boolean('is_backward');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movements');
+    }
+};
