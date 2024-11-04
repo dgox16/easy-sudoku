d891ebeafe7d901427b9046f31606a2d87415920
Diego Gomez <Diego.Gomez@pilsac.com>
2024-11-04 10:30:08 -0600

    Se cambio la fuente por Rubik

--- frontend/src/components/sudokuComponents/Sudoku.tsx
+++ frontend/src/components/sudokuComponents/Sudoku.tsx
@@ -13,7 +13,7 @@ export const Sudoku = () => {
 
     return (
         <div className="flex flex-col justify-center items-center bg-polar-night-0">
-            <p className="text-snow-storm-2 font-bold text-2xl mb-3">
+            <p className="text-snow-storm-2 font-medium text-2xl mb-3">
                 {formattedTime}
             </p>
             <div className="grid grid-cols-9 p-4 rounded-xl bg-polar-night-1">
