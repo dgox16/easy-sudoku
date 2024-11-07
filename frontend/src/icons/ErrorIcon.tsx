0d5e7a6565aa4c44079641b1a9b1004ea5d928f3
Diego Armando <dgox16@gmail.com>
2024-11-06 20:06:53 -0600

    Se cambio el icono de error a su propio archivo

---
+++ frontend/src/icons/ErrorIcon.tsx
@@ -0,0 +1,24 @@
+export const ErrorIcon = () => {
+    return (
+        <svg
+            fill="#BF616A"
+            viewBox="0 0 1920 1920"
+            xmlns="http://www.w3.org/2000/svg"
+            className="text-aurora-red size-5 mr-4"
+        >
+            <title>Error</title>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+            <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <g id="SVGRepo_iconCarrier">
+                <path
+                    d="M213.333 960c0-167.36 56-321.707 149.44-446.4L1406.4 1557.227c-124.693 93.44-279.04 149.44-446.4 149.44-411.627 0-746.667-335.04-746.667-746.667m1493.334 0c0 167.36-56 321.707-149.44 446.4L513.6 362.773c124.693-93.44 279.04-149.44 446.4-149.44 411.627 0 746.667 335.04 746.667 746.667M960 0C429.76 0 0 429.76 0 960s429.76 960 960 960 960-429.76 960-960S1490.24 0 960 0"
+                    fillRule="evenodd"
+                />
+            </g>
+        </svg>
+    );
+};
