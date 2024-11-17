c6ef5bad664769c720137d2238691d619ad08116
Diego Armando <dgox16@gmail.com>
2024-11-17 01:18:13 -0600

    Se agrego un tiempo de expiracion para no crear un juego nuevo siempre

--- frontend/src/store/useGameStore.ts
+++ frontend/src/store/useGameStore.ts
@@ -5,27 +5,34 @@ import type { GameType } from "../types/sudokuTypes.ts";
 interface GameState {
     game: GameType;
     timer: number;
+    createdAt: number;
     setGame: (newGame: GameType) => void;
     setTimer: (time: number) => void;
     incTimer: () => void;
+    isGameExpired: () => boolean;
 }
 
 export const useGameStore = create<GameState>()(
     persist(
-        (set) => ({
+        (set, get) => ({
             game: {
                 game: 0,
                 sudoku: [],
             },
             timer: 0,
+            createdAt: Date.now(),
             setGame: (newGame: GameType) => {
-                set({ game: newGame });
+                set({ game: newGame, createdAt: Date.now() });
             },
             setTimer: (time: number) => {
                 set({ timer: time });
             },
             incTimer: () => {
                 set((state) => ({ timer: state.timer + 1 }));
+            },
+            isGameExpired: () => {
+                const { createdAt } = get();
+                return Date.now() - createdAt > 300000;
             },
         }),
         {
--- frontend/src/utils/getGames.ts
+++ frontend/src/utils/getGames.ts
@@ -2,12 +2,21 @@ import type React from "react";
 import { newGameRequest } from "../services/sudokuRequests.ts";
 import type { GameType } from "../types/sudokuTypes.ts";
 import { convertMatrixToGrid } from "./formatSudoku.ts";
+import { useGameStore } from "../store/useGameStore.ts";
 
 export const fetchSudoku = async (
     setTimer: (time: number) => void,
     setGame: (newGame: GameType) => void,
     prevGameRef: React.MutableRefObject<GameType | null>,
 ) => {
+    const { game, isGameExpired } = useGameStore.getState();
+
+    if (!isGameExpired() && game.sudoku.length > 0) {
+        prevGameRef.current = game;
+        setGame(game);
+        return;
+    }
+
     try {
         setTimer(0);
         const gameResponse = await newGameRequest("medium");
